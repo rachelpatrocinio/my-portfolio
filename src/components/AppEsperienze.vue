f7ac22afc0afc155de5593c903e770e256ae4806
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-07-26 18:04:06 +0200

    ADD: AppEsperienze to AppHome

---
+++ src/components/AppEsperienze.vue
@@ -0,0 +1,81 @@
+<template>
+<section class="esperienze py-5">
+    <div class="container p-5 rounded">
+        <div class="row">
+            <h1 class="mb-4">Esperienze</h1>
+            <div class="col-12 mb-4">
+                <h2>BOOLEAN</h2>
+                <h5>JR FULL-STACK WEB DEVELOPER TRAINEE</h5>
+                <h6>Gennaio | Luglio 2024</h6>
+                <p>Il corso intensivo di Full Stack Web Developer mi ha permesso di acquisire competenze approfondite sia nello sviluppo front end che back end. Ho partecipato a lezioni teoriche, esercitazioni pratiche e progetti reali che mi hanno preparato a sviluppare applicazioni web complete e performanti.</p>
+
+                <div>
+                    <p><strong>Front End Development:</strong></p>
+                    <ul>
+                        <li><span>HTML5 & CSS3</span>: Creazione di layout responsivi e ottimizzati per vari dispositivi, utilizzo di framework come Bootstrap per velocizzare lo sviluppo.</li>
+                        <li><span>JavaScript</span>: Sviluppo di funzionalità interattive, gestione del DOM e manipolazione di eventi.</li>
+                        <li><span>Librerie e Frameworks</span>: Utilizzo di Vue.js per la creazione di interfacce utente dinamiche e modulari.</li>
+                        <li><span>Responsive Design</span>: Tecniche per garantire una user experience ottimale su dispositivi mobili e desktop.</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <p><strong>Back End Development:</strong></p>
+                    <ul>
+                        <li><span>PHP</span>: Sviluppo di applicazioni web dinamiche utilizzando PHP.</li>
+                        <li><span>Laravel</span>: Utilizzo del framework Laravel per lo sviluppo rapido di applicazioni, gestione di routing, middleware e servizi RESTful.</li>
+                        <li><span>Database</span>: Progettazione e gestione di database relazionali con MySQL, utilizzo di Eloquent ORM per l'interazione con il database.</li>
+                        <li><span>API Development</span>: Creazione e gestione di API RESTful per la comunicazione tra front end e back end.</li>
+                        <li><span>Autenticazione e Sicurezza</span>: Implementazione di sistemi di autenticazione e autorizzazione, protezione delle applicazioni contro vulnerabilità comuni.</li>
+                    </ul>
+                </div>
+
+            </div>
+            <div class="col-12">
+                <h2>CARPISA - DESIGUAL - ALCOTT</h2>
+                <h5>COMMESSA</h5>
+                <h6>2016 | 2022</h6>
+                <p>
+                    Grazie a questo lavoro ho maturato competenze nel <strong>lavoro in team</strong>,
+                    raggiungendo gli obiettivi prefissati e assegnatoci rispettandone le
+                    scadenze. Inoltre, ho sviluppato una capacità comunicativa
+                    efficace, dove sono stata in grado di comunicare in modo chiaro,
+                    preciso e veloce, rispondendo e soddisfacendo specifiche richieste
+                    della clientela.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.esperienze{
+
+    .container{
+        width: 80%;
+        background-color: #ae999a7e;
+
+        .row{
+            h2{
+                color: var(--main-color);
+            }
+
+            ul{
+                list-style:circle;
+                
+                span{
+                    font-weight: 500;
+                }
+            }
+        }
+    }
+}
+
+</style>
--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -36,54 +36,7 @@
     </div>
     <div class="main-bg py-5">
         <AppForm></AppForm>
-        <section class="esperienze py-5">
-            <div class="container p-5 rounded">
-                <div class="row">
-                    <h1 class="mb-4">Esperienze</h1>
-                    <div class="col-12 mb-4">
-                        <h2>BOOLEAN</h2>
-                        <h5>JR FULL-STACK WEB DEVELOPER TRAINEE</h5>
-                        <h6>Gennaio | Luglio 2024</h6>
-                        <p>Il corso intensivo di Full Stack Web Developer mi ha permesso di acquisire competenze approfondite sia nello sviluppo front end che back end. Ho partecipato a lezioni teoriche, esercitazioni pratiche e progetti reali che mi hanno preparato a sviluppare applicazioni web complete e performanti.</p>
-
-                        <div>
-                            <p><strong>Front End Development:</strong></p>
-                            <ul>
-                                <li><span>HTML5 & CSS3</span>: Creazione di layout responsivi e ottimizzati per vari dispositivi, utilizzo di framework come Bootstrap per velocizzare lo sviluppo.</li>
-                                <li><span>JavaScript</span>: Sviluppo di funzionalità interattive, gestione del DOM e manipolazione di eventi.</li>
-                                <li><span>Librerie e Frameworks</span>: Utilizzo di Vue.js per la creazione di interfacce utente dinamiche e modulari.</li>
-                                <li><span>Responsive Design</span>: Tecniche per garantire una user experience ottimale su dispositivi mobili e desktop.</li>
-                            </ul>
-                        </div>
-
-                        <div>
-                            <p><strong>Back End Development:</strong></p>
-                            <ul>
-                                <li><span>PHP</span>: Sviluppo di applicazioni web dinamiche utilizzando PHP.</li>
-                                <li><span>Laravel</span>: Utilizzo del framework Laravel per lo sviluppo rapido di applicazioni, gestione di routing, middleware e servizi RESTful.</li>
-                                <li><span>Database</span>: Progettazione e gestione di database relazionali con MySQL, utilizzo di Eloquent ORM per l'interazione con il database.</li>
-                                <li><span>API Development</span>: Creazione e gestione di API RESTful per la comunicazione tra front end e back end.</li>
-                                <li><span>Autenticazione e Sicurezza</span>: Implementazione di sistemi di autenticazione e autorizzazione, protezione delle applicazioni contro vulnerabilità comuni.</li>
-                            </ul>
-                        </div>
-
-                    </div>
-                    <div class="col-12">
-                        <h2>CARPISA - DESIGUAL - ALCOTT</h2>
-                        <h5>COMMESSA</h5>
-                        <h6>2016 | 2022</h6>
-                        <p>
-                            Grazie a questo lavoro ho maturato competenze nel <strong>lavoro in team</strong>,
-                            raggiungendo gli obiettivi prefissati e assegnatoci rispettandone le
-                            scadenze. Inoltre, ho sviluppato una capacità comunicativa
-                            efficace, dove sono stata in grado di comunicare in modo chiaro,
-                            preciso e veloce, rispondendo e soddisfacendo specifiche richieste
-                            della clientela.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <AppEsperienze></AppEsperienze>
     </div>
 </template>
 
@@ -92,6 +45,8 @@ import AppHero from '../components/AppHero.vue'
 import AppCompetenze from '../components/AppCompetenze.vue'
 import AppProgetti from '../components/AppProgetti.vue'
 import AppForm from '../components/AppForm.vue'
+import AppEsperienze from '../components/AppEsperienze.vue'
+
 
 import { store } from "../store.js";
 
@@ -100,7 +55,8 @@ export default {
         AppHero,
         AppCompetenze,
         AppProgetti,
-        AppForm
+        AppForm,
+        AppEsperienze
     },
     data(){
         return{
@@ -137,40 +93,23 @@ export default {
             background-color: var(--main-color);
         }
     }
-}
 
-.socials{
+    .socials{
 
-    a.social{
-        text-decoration: none;
-        color: var(--main-dark-color);
-        margin-bottom: 10px;
-        font-size: 18px;
+        a.social{
+            text-decoration: none;
+            color: var(--main-dark-color);
+            margin-bottom: 10px;
+            font-size: 18px;
 
-        &:hover{
-            color: var(--main-color);
+                &:hover{
+                    color: var(--main-color);
+                }
         }
     }
 }
 
-// .formazione{
 
-//     .container{
-//         width: 70%;
-//     }
-
-//     h2{
-//         color: var(--main-dark-color);
-//     }
-
-//     .logo{
-//         width: 200px;
-//         aspect-ratio: 1/1;
-//         object-fit: cover;
-//         border-radius: 100%;
-//         box-shadow: 0 8px 8px -4px var(--dark-color);
-//     }
-// }
 
 .dark-bg{
     color: white;
@@ -182,62 +121,6 @@ export default {
                 transition: all 0.8s;
             }       
         }     
-    }
-}
-
-.certificato{
-  width: 100%;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 99;
-  background-color: rgba(0, 0, 0, 0.734);
-
-  .certificato-img {
-    width: 40%;
-    background-color: white;
-    position: relative;
-
-    img{
-        height: 600px;
-    }
-  }
-}
-
-.close{
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    font-size: 40px;
-    color: var(--dark-color);
-    cursor: pointer;
-}
-
-
-.esperienze{
-
-    .container{
-        width: 80%;
-        background-color: #ae999a7e;
-
-        .row{
-            h2{
-                color: var(--main-color);
-            }
-
-            ul{
-                list-style:circle;
-                
-                span{
-                    font-weight: 500;
-                }
-            }
-        }
     }
 }
 
