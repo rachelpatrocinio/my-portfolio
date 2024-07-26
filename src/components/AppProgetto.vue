73fa6764b8d6166a98d32a4764c75520a14f0871
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-07-27 00:43:55 +0200

    ADD: 1 more project

--- src/components/AppProgetto.vue
+++ src/components/AppProgetto.vue
@@ -3,11 +3,11 @@
     <div class="container">
         <h1 class="mb-3 mb-md-5">I miei progetti in Boolean</h1>
         <div class="row m-1 m-md-0">
-            <div class="col-12 col-md-4" v-for="progetto in store.progetti" :key="progetto.id">
+            <div class="col-12 col-md-4" v-for="progetto in store.progettiHome" :key="progetto.id">
                 <AppCard :progetto="progetto"></AppCard>
             </div>
             <div class="col-12 text-white">
-                <RouterLink to="/progetti" class="router more">. . .</RouterLink>
+                <RouterLink @click="store.windowScroll()" to="/progetti" class="router more">. . . vedi altri</RouterLink>
             </div>
         </div>
     </div>
@@ -26,6 +26,14 @@ import { store } from "../store.js";
             return{
                 store
             }
+        },
+        methods:{
+            sixProj(){
+                store.progettiHome = store.progetti.filter((element, index) => index < 6);
+            }
+        },
+        created(){
+            this.sixProj()
         }
     }
 </script>
@@ -42,6 +50,14 @@ import { store } from "../store.js";
 
         &:hover{
             color: var(--main-dark-color);
+        }
+    }
+}
+
+@media screen and (max-width: 789px){
+    .progetti{
+        h1{
+            text-align: center;
         }
     }
 }
--- src/pages/AppProgetti.vue
+++ src/pages/AppProgetti.vue
@@ -1,17 +1,44 @@
 <template>
     <div class="container py-5">
         <div class="row py-5">
-            PROGETTI
+            <h1 class="my-4">I miei progetti</h1>
+            <div class="col-12 col-md-4" v-for="progetto in store.progetti">
+                <AppCard  :progetto="progetto"></AppCard>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        
+import AppCard from '../components/AppCard.vue'
+import { store } from "../store.js";
+ 
+export default {
+    components:{
+        AppCard
+    },
+    data(){
+        return{
+            store
+        }
     }
+}
 </script>
 
 <style lang="scss" scoped>
+.container{
+    width: 80%;
+}
 
+@media screen and (max-width: 789px) {
+    .container{
+        width: 100%;
+
+        .row{
+            h1{
+                text-align: center;
+            }
+        }
+    }
+}
 </style>
